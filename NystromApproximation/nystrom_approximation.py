8bc5e22c04dce69a83cf349e95671a68280b8948
Orleg01 <orleg01@gmail.com>
2024-04-26 16:48:59 +0300

    start approximation code

---
+++ NystromApproximation/nystrom_approximation.py
@@ -0,0 +1,15 @@
+
+from typing import List
+
+
+class NystromApproximation:
+
+    def __init__(self, E: List[List[int]], F: List[List[int]]):
+        """
+
+        :param E: landmark known matrix m * m
+        :param F: landmark to other matrix m * (k = n - m)
+        """
+
+        
+

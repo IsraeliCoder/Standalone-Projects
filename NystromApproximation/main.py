60b4fbb310b346adbf675d11316db7fba3d1d8c9
Orleg01 <orleg01@gmail.com>
2024-04-29 18:17:36 +0300

    MDS \ LMDS implemented

--- NystromApproximation/main.py
+++ NystromApproximation/main.py
@@ -1,8 +1,14 @@
 
+
+import numpy as np
+
 from nystrom_approximation import NystromApproximation
 from graph import Graph
 import random
 random.seed(250892)
+
+from mds import MDS, landmark_MDS
+import matplotlib.pyplot as plt
 
 
 def print_matrix(M):
@@ -12,9 +18,23 @@ def print_matrix(M):
 
     print("\n\n")
 
-
 graph = Graph(20, 3)
 distance_graph = graph.get_distance_matrix()
+
+if True:
+    lands = np.array([0, 1, 2, 3, 4, ],dtype=int)
+
+    x2 = landmark_MDS(np.array(distance_graph)[lands, :], lands= lands, dim=2)
+    plt.figure()
+    colors = ['navy', 'turquoise', 'darkorange']
+
+
+    plt.scatter(x2[:,0], x2[:,1], alpha=.8)
+    plt.legend(loc='best', shadow=False, scatterpoints=1)
+    plt.title('MDS of IRIS dataset')
+    plt.show()
+
+    exit()
 
 print("Distance Matrix")
 print_matrix(distance_graph)
@@ -27,4 +47,25 @@ print_matrix(nystrom.A)
 print("B")
 print_matrix(nystrom.B)
 
+nystrom.calculateC()
+print("C result")
+print_matrix(nystrom.C)
 
+X,Y = nystrom.calculateXY()
+
+#print(2)
+
+#exit()
+
+K = nystrom.get_full_matrix()
+print("K matrix")
+print_matrix(K)
+
+def euclidean_dist_from_gram(gram):
+    temp = np.tile(np.diag(gram), (nystrom.m + nystrom.k,1))
+    return temp + temp.T - 2*gram
+
+g = np.tile(np.expand_dims(np.diag(K), 0), np.array([len(K),1]))
+euclidean_dist = g + np.transpose(g) - 2*np.array(K)
+
+print_matrix(euclidean_dist_from_gram(np.array(K)).tolist())
--- NystromApproximation/nystrom_approximation.py
+++ NystromApproximation/nystrom_approximation.py
@@ -1,5 +1,7 @@
 
 from typing import List
+import numpy as np
+
 
 def sum_matrix_horizontal(M: List[List[int]], i: int, m: int):
 
@@ -48,8 +50,8 @@ class NystromApproximation:
         self.A = A = [[0.0 for _ in range(m)] for _ in range(m)]
         self.B = B = [[0.0 for _ in range(k)] for _ in range(m)]
 
-        self.__setA__(A)
-        self.__setB__(B)
+        self.__setA__()
+        self.__setB__()
 
     def __setA_helper__(self, i: int, j: int) -> float:
 
@@ -62,11 +64,11 @@ class NystromApproximation:
 
         return -0.5 * result
 
-    def __setA__(self, A: List[List[float]]):
+    def __setA__(self):
 
         for i in range(self.m):
             for j in range(self.m):
-                A[i][j] = self.__setA_helper__(i, j)
+                self.A[i][j] = self.__setA_helper__(i, j)
 
     def __setB_helper__(self, i: int, j: int) -> float:
 
@@ -79,9 +81,49 @@ class NystromApproximation:
 
         return -0.5 * result
 
-    def __setB__(self, B: List[List[float]]):
+    def __setB__(self):
 
         for i in range(self.m):
             for j in range(self.k):
-                B[i][j] = self.__setB_helper__(i, j)
+                self.B[i][j] = self.__setB_helper__(i, j)
 
+    def calculateC(self):
+
+        A = np.array(self.A)
+        B = np.array(self.B)
+
+        A_INV = np.linalg.pinv(A)
+
+        C = np.matmul(np.matmul(B.T, A_INV), B)
+        self.C = C.tolist()
+
+        return self.C
+
+    def calculateXY(self):
+
+        eigen_values, eigen_vectors = np.linalg.eig(np.array(self.A))
+
+        X = np.dot(np.power(eigen_values, 0.5), eigen_vectors.T)
+        Y = np.dot(np.dot(np.power(eigen_values, -0.5), eigen_vectors.T), np.array(self.B))
+
+        return X, Y
+
+    def get_full_matrix(self) -> List[List[float]]:
+
+        n = self.m + self.k
+        new_arr = [[0.0 for _ in range(n)] for _ in range(n)]
+
+        for i in range(self.m):
+            for j in range(self.m):
+                new_arr[i][j] = self.A[i][j]
+
+        for i in range(self.m):
+            for j in range(self.k):
+                new_arr[i][self.m + j] = self.B[i][j]
+                new_arr[self.m + j][i] = self.B[i][j]
+
+        for i in range(self.k):
+            for j in range(self.k):
+                new_arr[self.m + i][self.m + j] = self.C[i][j]
+
+        return new_arr
